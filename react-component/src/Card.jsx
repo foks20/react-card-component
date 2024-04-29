88afbf8c154e66d451c23a7b051d90d11e88526e
foks20 <samuelfok2013@yahoo.com>
2024-04-29 10:23:42 -0400

    react-card component

---
+++ react-component/src/Card.jsx
@@ -0,0 +1,15 @@
+
+
+function Card() {
+    return (
+        <>
+            <div className="card">
+                <img className="imageProfile" src="https://via.placeholder.com/150" alt="image place holder"></img>
+                <h1>Samuel</h1>
+                <p>Hello my friend</p>
+            </div>
+        </>
+    )
+}
+
+export default Card
